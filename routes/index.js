9500aa3f84d1aa0659c48dfdc6318fe233d11169
zadipgm <gm.zadip@gmail.com>
2023-03-02 15:19:44 +0300

    comment code for testing login

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,17 @@
 import express from "express";
-import {
-  getUsers,
-  Register,
-  Login,
-  Logout,
-  deleteUser,
-  editUser,
-  updateUser,
-} from "../controllers/Users.js";
-import { verifyToken } from "../middleware/VerifyToken.js";
-import { refreshToken } from "../controllers/RefreshToken.js";
-import { SetHeadTag, getHeadTag } from "../controllers/Head.js";
-import { getZadipFormData } from "../controllers/ZadipForm.js";
+// import {
+//   getUsers,
+//   Register,
+//   Login,
+//   Logout,
+//   deleteUser,
+//   editUser,
+//   updateUser,
+// } from "../controllers/Users.js";
+// import { verifyToken } from "../middleware/VerifyToken.js";
+// import { refreshToken } from "../controllers/RefreshToken.js";
+// import { SetHeadTag, getHeadTag } from "../controllers/Head.js";
+// import { getZadipFormData } from "../controllers/ZadipForm.js";
 const router = express.Router();
 
 router.get("/", (req, res) => {
