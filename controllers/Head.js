1cd2dea9df05aedeb14d0c2193881a786a4b20fd
zadipgm <gm.zadip@gmail.com>
2023-03-02 15:18:24 +0300

    comment code for testing controller

--- controllers/Head.js
+++ controllers/Head.js
@@ -1,44 +1,44 @@
-import HeadTag from "../models/HeadModel.js";
-export const SetHeadTag = async (req, res) => {
-  console.log("here is body", req.body);
-  const {
-    Page_Title,
-    Meta_Name,
-    Meta_Description,
-    Meta_Property,
-    Meta_Property_Description,
-    Page_Name,
-  } = req.body;
-  try {
-    await HeadTag.create({
-      Page_Title: Page_Title,
-      Meta_Name: Meta_Name,
-      Meta_Description: Meta_Description,
-      Meta_Property: Meta_Property,
-      Meta_Property_Description: Meta_Property_Description,
-      Page_Name: Page_Name,
-    });
-    res.json({ message_en: "Data Added Successfully!" });
-  } catch (error) {
-    console.log(error);
-  }
-};
+// import HeadTag from "../models/HeadModel.js";
+// export const SetHeadTag = async (req, res) => {
+//   console.log("here is body", req.body);
+//   const {
+//     Page_Title,
+//     Meta_Name,
+//     Meta_Description,
+//     Meta_Property,
+//     Meta_Property_Description,
+//     Page_Name,
+//   } = req.body;
+//   try {
+//     await HeadTag.create({
+//       Page_Title: Page_Title,
+//       Meta_Name: Meta_Name,
+//       Meta_Description: Meta_Description,
+//       Meta_Property: Meta_Property,
+//       Meta_Property_Description: Meta_Property_Description,
+//       Page_Name: Page_Name,
+//     });
+//     res.json({ message_en: "Data Added Successfully!" });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-export const getHeadTag = async (req, res) => {
-  try {
-    const meta = await HeadTag.findAll({
-      attributes: [
-        "id",
-        "Page_Title",
-        "Meta_Name",
-        "Meta_Description",
-        "Meta_Property",
-        "Meta_Property_Description",
-        "Page_Name",
-      ],
-    });
-    res.json(meta);
-  } catch (error) {
-    console.log(error);
-  }
-};
+// export const getHeadTag = async (req, res) => {
+//   try {
+//     const meta = await HeadTag.findAll({
+//       attributes: [
+//         "id",
+//         "Page_Title",
+//         "Meta_Name",
+//         "Meta_Description",
+//         "Meta_Property",
+//         "Meta_Property_Description",
+//         "Page_Name",
+//       ],
+//     });
+//     res.json(meta);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
