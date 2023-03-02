f08f5a68356b45a60525adcb7416b57c553232c5
zadipgm <gm.zadip@gmail.com>
2023-03-02 15:16:41 +0300

    comment code for testing head model

--- models/HeadModel.js
+++ models/HeadModel.js
@@ -1,37 +1,37 @@
-import { Sequelize } from "sequelize";
-import db from "../config/Database.js";
+// import { Sequelize } from "sequelize";
+// import db from "../config/Database.js";
 
-const { DataTypes } = Sequelize;
+// const { DataTypes } = Sequelize;
 
-const HeadTag = db.define(
-  "head_tag",
-  {
-    Page_Title: {
-      type: DataTypes.STRING,
-    },
-    Meta_Name: {
-      type: DataTypes.STRING,
-    },
-    Meta_Description: {
-      type: DataTypes.STRING,
-    },
-    Meta_Property: {
-      type: DataTypes.STRING,
-    },
-    Meta_Property_Description: {
-      type: DataTypes.STRING,
-    },
-    Page_Name: {
-      type: DataTypes.STRING,
-    },
-  },
-  {
-    freezeTableName: true,
-  }
-);
+// const HeadTag = db.define(
+//   "head_tag",
+//   {
+//     Page_Title: {
+//       type: DataTypes.STRING,
+//     },
+//     Meta_Name: {
+//       type: DataTypes.STRING,
+//     },
+//     Meta_Description: {
+//       type: DataTypes.STRING,
+//     },
+//     Meta_Property: {
+//       type: DataTypes.STRING,
+//     },
+//     Meta_Property_Description: {
+//       type: DataTypes.STRING,
+//     },
+//     Page_Name: {
+//       type: DataTypes.STRING,
+//     },
+//   },
+//   {
+//     freezeTableName: true,
+//   }
+// );
 
-(async () => {
-  await db.sync();
-})();
+// (async () => {
+//   await db.sync();
+// })();
 
-export default HeadTag;
+// export default HeadTag;
