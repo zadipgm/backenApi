a23031a55e52ec0cbde19825c28c7dd97c899d5e
zadipgm <gm.zadip@gmail.com>
2023-03-05 09:59:14 +0300

    db import

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-const Sequelize = require("sequelize");
+import { Sequelize } from "sequelize";
 const db = new Sequelize("zadip_sa", "root", "Shani@786!@#", {
   host: "localhost",
   dialect: "mysql",
