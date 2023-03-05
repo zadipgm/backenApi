8ba94d3a10c6bd760ec0871cbb7ac5d2c891531b
zadipgm <gm.zadip@gmail.com>
2023-03-05 16:38:14 +0300

    cors issues

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ import cors from "cors";
 import router from "./routes/index.js";
 dotenv.config();
 const app = express();
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 app.use(
   cors({
     credentials: true,
