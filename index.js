fef0d98fb48ccb41978bc4b1e79ad789f2b867e9
zadipgm <gm.zadip@gmail.com>
2023-03-05 16:42:29 +0300

    cors issues

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(function (req, res, next) {
 app.use(
   cors({
     credentials: true,
-    origin: "https://zadip.sa",
+    origin: ["https://zadip.sa", "https://zadip-dashboardv2.netlify.app"],
   })
 );
 app.use(cookieParser());
